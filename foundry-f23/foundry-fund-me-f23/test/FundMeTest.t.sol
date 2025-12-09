3f2298ab390ff42607907534b009ec70725e6d73
“yjl000” <623139771@qq.com>
2025-12-09 23:41:40 +0800

    feat: learn write test contract and create fundme html

---
+++ foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe public fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        console.log(fundMe.i_owner());
+        console.log(msg.sender);
+        console.log(address(this));
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
