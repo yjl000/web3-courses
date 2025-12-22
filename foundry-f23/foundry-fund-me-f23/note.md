a80b83c315695ab36f2e4635068e589ed0adad31
“yjl000” <623139771@qq.com>
2025-12-22 23:10:47 +0800

    feat: learn foundry Cheatcodes

--- foundry-f23/foundry-fund-me-f23/note.md
+++ foundry-f23/foundry-fund-me-f23/note.md
@@ -4,3 +4,12 @@
 3、forge test --mt testFunctionName --fork-url $SEPOLIA_RPC_URL
 
 测试覆盖率：forge coverage --fork-url $SEPOLIA_RPC_URL
+
+Cheatcodes:
+
+1、vm.expectRevert() // 期望下一个交易会revert
+2、vm.prank() // 设置为下一次调用的指定地址
+3、vm.startPrank() // 设置为下一次调用的指定地址, 直到调用vm.stopPrank()才会恢复
+4、vm.stopPrank() // 停止startPrank()的设置。
+5、makeAddr() // 创建新的用户地址
+6、vm.deal(alice, 100 ether) // 为alice设置初始余额
--- foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
+++ foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -8,10 +8,14 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe public fundMe;
     DeployFundMe deployFundMe;
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    address alice = makeAddr("alice");
 
     function setUp() external {
         deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run(); // vm.startBroadcast();将测试合约地址改为了发送者的地址，即DeployFundMe的地址
+        vm.deal(alice, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public view {
@@ -28,5 +32,17 @@ contract FundMeTest is Test {
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // 期望下一个交易会revert
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundDataStructure() public {
+        vm.prank(alice);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(alice);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
