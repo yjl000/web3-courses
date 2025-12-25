d13c348109b5d045653a7ab39da62a73aa512629
“yjl000” <623139771@qq.com>
2025-12-26 00:02:11 +0800

    feat: Create Integration Tests

--- foundry-f23/foundry-fund-me-f23/test/uint/FundMeTest.t.sol
+++ foundry-f23/foundry-fund-me-f23/test/uint/FundMeTest.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
-import {FundMe} from "../src/FundMe.sol";
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe public fundMe;
