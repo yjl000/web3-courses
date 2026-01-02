38becbf9d66aa22f2e8804ec420b0d58fd3bd89f
“yjl000” <623139771@qq.com>
2026-01-03 00:15:05 +0800

    fix: import chainlink and request random words

--- foundry-f23/foundry-smart-contract-lottery-f23/src/Raffle.sol
+++ foundry-f23/foundry-smart-contract-lottery-f23/src/Raffle.sol
@@ -1,15 +1,3 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-/**
- * @title A sample Raffle Contract * @title 一个示例抽奖合约
- * @author yangjingli
- * @notice This contract is for creating a sample raffle
- * @notice 本合约用于创建一个示例抽奖活动
- * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
- * @dev 它实现了Chainlink VRFv2.5和Chainlink自动化功能
- */
-
 // Layout of the contract file:
 // version
 // imports
@@ -33,21 +21,83 @@ pragma solidity ^0.8.24;
 // private
 // view & pure functions
 
-error Raffle__NotEnoughEthSend();
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import {VRFCoordinatorV2Interface} from "chainlink/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "chainlink/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
-contract Raffle {
+/**
+ * @title A sample Raffle Contract * @title 一个示例抽奖合约
+ * @author yangjingli
+ * @notice This contract is for creating a sample raffle
+ * @notice 本合约用于创建一个示例抽奖活动
+ * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
+ * @dev 它实现了Chainlink VRFv2.5和Chainlink自动化功能
+ */
+
+contract Raffle is VRFConsumerBaseV2 {
+    /** Errors */
+    error Raffle__NotEnoughEthSend();
+
+    /** State Variables */
+    // Chainlink VRF related variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
+
+    // Lottery Variables
     uint256 private immutable i_entranceFee;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
-    constructor(uint256 entranceFee) {
+    /** Event */
+    event EnteredRaffle(address indexed player);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGaslimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGaslimit;
     }
 
     /** 用external 因为只有外部调用，合约内部步调用此函数 */
     function enterRaffle() external payable {
         if (msg.value < i_entranceFee) revert Raffle__NotEnoughEthSend();
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() public {
+        if (block.timestamp - s_lastTimeStamp < i_interval) revert();
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
     /** Getter Function */
     function getEntranceFee() public view returns (uint256) {
